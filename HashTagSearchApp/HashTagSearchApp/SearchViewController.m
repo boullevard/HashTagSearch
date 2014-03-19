a65cda30865736fe903e3e07cdec673d855b5bdf
Nabil Mouzannar <info@boullevard.com>
2014-03-19 12:30:45 -0400

    Added NS Log to Search View
    to see if Search view loaded

--- HashTagSearchApp/HashTagSearchApp/SearchViewController.m
+++ HashTagSearchApp/HashTagSearchApp/SearchViewController.m
@@ -28,6 +28,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    NSLog(@"my Search View controller Loaded");
 }
 
 - (void)didReceiveMemoryWarning
