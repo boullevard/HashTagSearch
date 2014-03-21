a4a312d3f53c53da749dd38a363f5c37342f0d7b
Nabil Mouzannar <info@boullevard.com>
2014-03-20 22:40:54 -0400

    Finished with saving Searches in NSUserDefaults
    Each time the user enters a search term and taps the search button,
    save the search term in NSUserDefaults and display the list of search
    terms in the tableview

--- HashTagSearchApp/HashTagSearchApp/SearchViewController.m
+++ HashTagSearchApp/HashTagSearchApp/SearchViewController.m
@@ -8,11 +8,12 @@
 
 #import "SearchViewController.h"
 
-@interface SearchViewController () <UISearchBarDelegate>
+@interface SearchViewController () <UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, strong) NSString *userInputSearchText;
-@property (nonatomic, strong) NSString *userSavedInputSearchText;
-
+@property (nonatomic, strong) NSMutableArray *searchTextArray ;
+@property (nonatomic, strong) IBOutlet UITableView *tableView; //link this file to the table view
+@property (nonatomic, strong) IBOutlet UISearchBar *searchBar; //link this file to the SearchBar view
 @end
 
 @implementation SearchViewController
@@ -21,8 +22,15 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
-          NSLog(@"retrieveSearchTextFromNSUSerDefaults %@",self.retrieveSearchTextFromNSUSerDefaults);
+        //initialize a NSMutable array to hold the last 10 searches
+        self.searchTextArray = [[NSMutableArray alloc] initWithCapacity:10];
+        
+        if (self.retrieveSearchTextFromNSUSerDefaults != NULL)
+        {
+            NSLog(@"retrieveSearchTextFromNSUSerDefaults %i",[self.retrieveSearchTextFromNSUSerDefaults count]);
+            self.searchTextArray = self.retrieveSearchTextFromNSUSerDefaults;
+        }
+
     }
     return self;
 }
@@ -31,27 +39,67 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    NSLog(@"when did the view Load ?");
-
 }
 
-- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{ // called when keyboard search button pressed
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    // called when keyboard search button pressed
     self.userInputSearchText = searchBar.text;
-    [self saveSearchTextToNSUSerDefaults: self.userInputSearchText];
+    
+    //Call to saveSearchText to the saveSearchTExtArray
+    [self saveSearchTextToArray: self.userInputSearchText];
+    [searchBar resignFirstResponder];//dismisses keyboard
+    
 }
 
-- (void) saveSearchTextToNSUSerDefaults:(NSString *) searchText{
+- (void) saveSearchTextToArray:(NSString *) searchText
+{
+    //add searchTExt to searchTextArray
+    [self.searchTextArray addObject:searchText];
+    //and then store it in nsuserdefaults
+    
+    //update tableview
+    [self.tableView reloadData];
+    
+    //Save user searches to NSUserDefaults
+    [self saveSearchTextToNSUSerDefaults: self.searchTextArray];
+}
+- (void) saveSearchTextToNSUSerDefaults:(NSArray *) searchTextArray
+{
     NSUserDefaults *searches = [NSUserDefaults standardUserDefaults]; //why couldnt i init this in viewDidLoad
-    [searches setObject:searchText forKey:@"searchText"];
+    [searches setObject:searchTextArray forKey:@"searchTextArray"];
     [searches synchronize];
-    NSLog(@"saveSearchTextToNSUSerDefaults userInputSearchText: %@.",searchText);
+    NSLog(@"saveSearchTextToNSUSerDefaults userInputSearchText: %i.",[searchTextArray count]);
 }
 
-- (NSString *) retrieveSearchTextFromNSUSerDefaults {
+- (NSMutableArray *) retrieveSearchTextFromNSUSerDefaults {
     NSUserDefaults *searches = [NSUserDefaults standardUserDefaults];
-    self.userSavedInputSearchText = [searches stringForKey:@"searchText"];
-    return self.userSavedInputSearchText;
+    return [searches objectForKey:@"searchTextArray"];
 }
+
+//required functions (2) for UITableViewDataSource
+#pragma mark - UITableView Datasource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+
+    cell.textLabel.text = self.searchTextArray[indexPath.row];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.searchTextArray count]; //return size of the array
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
